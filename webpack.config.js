76c0c64d0cb7dce0efd3079a69c114917e48cb9d
Sumit Govil <sumg.work@gmail.com>
2022-08-11 18:57:15 +1000

    build(webpack): using webpack for bundling

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "production",
+  output: {
+    globalObject: "this",
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "react-tailwind-component-library",
+    libraryTarget: "umd",
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      //   { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  // plugins: [new BundleAnalyzerPlugin()],
+};
