612c1078a5802eeda3f369424d94ab7661942f51
Sumit Govil <sumg.work@gmail.com>
2022-08-16 14:16:35 +1000

    build(theme context): exporting theme context and config from the package

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { ThemeContext as ReactTailwindComponentLibraryContext } from './context/ThemeContext';
+export { default as ReactTailwindComponentLibraryDS } from './ReactTailwindComponentLibraryDS';
+
 export * from './components/Text';
 export * from './components/Icon';
